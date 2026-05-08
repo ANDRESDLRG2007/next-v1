b8c3f52e44eee2ed90c8c09bf64a3dcb6abd5561
Andres DLRG <cristian.rueg@uniagustiniana.edu.co>
2026-05-08 18:58:28 +0000

    Clase 1: next estructure

---
+++ app/dos/page.tsx
@@ -0,0 +1,12 @@
+import Hijo from './hijo'
+
+export default function Dos() {
+  return (
+    <div>
+      <br />
+        <Hijo>
+            dos
+        </Hijo>
+    </div>
+  );
+}
---
+++ app/uno/page.tsx
@@ -0,0 +1,11 @@
+import Props from "./props";
+export default function Uno() {
+  
+  return (
+    <div>
+      <br />
+      <p>Uno :)</p>
+      <Props nombre={"este es un prop"}/>
+    </div>
+  );
+}
